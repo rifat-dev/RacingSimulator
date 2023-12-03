16a9f897090d4926c5133e968a5e97e939a5c6fd
Rifat <rifat0531m@gmail.com>
2023-12-03 22:34:24 +0300

    Added blank test

---
+++ src/test/java/com/rifat/racingSimulator/RacingSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rifat.racingSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacingSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
